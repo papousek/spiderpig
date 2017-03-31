93798c46160075e8e7e3d198acbcbd6736136740
Jan Papousek <jan.papousek@gmail.com>
2017-03-31 13:35:28 +0200

    add "if main" to crawler because of testing

--- example/crawler.py
+++ example/crawler.py
@@ -3,8 +3,8 @@ from spiderpig import run_cli
 import general.commands
 import wikipedia.commands
 
-
-run_cli(
-    command_packages=[general.commands],
-    namespaced_command_packages={'wiki': wikipedia.commands}
-)
+if __name__ == "__main__":
+    run_cli(
+        command_packages=[general.commands],
+        namespaced_command_packages={'wiki': wikipedia.commands}
+    )
